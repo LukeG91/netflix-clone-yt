c1b779a9e4875964e574fe8ed6b7436bc2d009e9
Luke Glendining <lukeglendining@gmail.com>
2021-02-04 23:34:34 +0200

    newest commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,10 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDSC0ZqjCNoBOPi3NF4GxxrXWPm5wOZtTs",
-    authDomain: "netflix-clone-394d3.firebaseapp.com",
-    projectId: "netflix-clone-394d3",
-    storageBucket: "netflix-clone-394d3.appspot.com",
-    messagingSenderId: "528644598039",
-    appId: "1:528644598039:web:5bed414b748ebd61246f52"
-  };
+  apiKey: "AIzaSyDSC0ZqjCNoBOPi3NF4GxxrXWPm5wOZtTs",
+  authDomain: "netflix-clone-394d3.firebaseapp.com",
+  projectId: "netflix-clone-394d3",
+  storageBucket: "netflix-clone-394d3.appspot.com",
+  messagingSenderId: "528644598039",
+  appId: "1:528644598039:web:5bed414b748ebd61246f52",
+};
