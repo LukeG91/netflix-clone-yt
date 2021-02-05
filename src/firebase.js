a05f4c76519070894b66f79201fc8a9d435ec1e8
Luke Glendining <lukeglendining@gmail.com>
2021-02-05 17:55:49 +0200

    login and authentication complete

--- src/firebase.js
+++ src/firebase.js
@@ -8,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "528644598039",
   appId: "1:528644598039:web:5bed414b748ebd61246f52",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
