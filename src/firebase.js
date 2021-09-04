6937ba3d087ec29f6e337a9735acba2f5dfc8a81
Luke Glendining <lukeglendining@gmail.com>
2021-09-04 11:54:13 +0200

    Added a file to .gitignore.

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,12 @@
 import firebase from "firebase";
 
+// const apiKey = process.env.API_KEY;
+// const authDomain = process.env.AUTH_DOMAIN;
+// const projectId = process.env.PROJECT_ID;
+// const storageBucket = process.env.STORAGE_BUCKET;
+// const messagingSenderId = process.env.MESSAGING_SENDER_ID;
+// const appId = process.env.APP_ID;
+
 const firebaseConfig = {
   apiKey: "AIzaSyDSC0ZqjCNoBOPi3NF4GxxrXWPm5wOZtTs",
   authDomain: "netflix-clone-394d3.firebaseapp.com",
